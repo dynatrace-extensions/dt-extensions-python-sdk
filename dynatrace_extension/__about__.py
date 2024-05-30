dynatrace-extensions/dt-extensions-python-sdk

commit daf08b685bc8566d751afc5590527844f980963e

initial commit (#50)

dynatrace_extension/sdk/communication.py
@@ -10,11 +10,12 @@ import random
 import sys
 import time
 from abc import ABC, abstractmethod
+from collections import deque
 from dataclasses import dataclass
 from enum import Enum
 from itertools import islice
 from pathlib import Path
-from typing import Any, Iterable, List, TypeVar
+from typing import Any, Iterable, List, TypeVar, Union
 
 from .vendor.mureq.mureq import HTTPException, Response, request
 
@@ -22,6 +23,8 @@ CONTENT_TYPE_JSON = "application/json;charset=utf-8"
 CONTENT_TYPE_PLAIN = "text/plain;charset=utf-8"
 COUNT_METRIC_ITEMS_DICT = TypeVar("COUNT_METRIC_ITEMS_DICT", str, List[str])
 MAX_MINT_LINES_PER_REQUEST = 1000
+MAX_LOG_EVENTS_PER_REQUEST = 50_000
+MAX_LOG_REQUEST_SIZE = 5_000_000
 HTTP_BAD_REQUEST = 400
 
 
@@ -94,7 +97,7 @@ class CommunicationClient(ABC):
         pass
 
     @abstractmethod
-    def send_events(self, event: dict | list[dict], eec_enrichment: bool) -> dict | None:
+    def send_events(self, event: dict | list[dict], eec_enrichment: bool) -> List[Union[dict | None]]:
         pass
 
     @abstractmethod
@@ -283,19 +286,25 @@ class HttpClient(CommunicationClient):
             responses.append(mint_response)
         return responses
 
-    def send_events(self, events: dict | list[dict], eec_enrichment: bool = True) -> dict | None:
+    def send_events(self, events: dict | list[dict], eec_enrichment: bool = True) -> List[Union[dict, None]]:
         self.logger.debug(f"Sending log events: {events}")
-        event_data = json.dumps(events).encode("utf-8")
-        try:
-            # EEC returns empty body on success
-            return self._make_request(
-                self._events_url,
-                "POST",
-                event_data,
-                extra_headers={"Content-Type": CONTENT_TYPE_JSON, "eec-enrichment": str(eec_enrichment).lower()},
-            ).json()
-        except json.JSONDecodeError:
-            return None
+        
+        responses = []
+        batches = divide_logs_into_batches([events] if type(events) == dict else events)
+
+        for batch in batches:
+            try:
+                # EEC returns empty body on success
+                responses.append(self._make_request(
+                    self._events_url,
+                    "POST",
+                    batch,
+                    extra_headers={"Content-Type": CONTENT_TYPE_JSON, "eec-enrichment": str(eec_enrichment).lower()},
+                ).json())
+            except json.JSONDecodeError:
+                responses.append(None)
+        
+        return responses
 
     def send_sfm_metrics(self, mint_lines: list[str]) -> MintResponse:
         mint_data = "\n".join(mint_lines).encode("utf-8")
@@ -448,6 +457,46 @@ def divide_into_chunks(iterable: Iterable, chunk_size: int) -> Iterable:
             return
         yield subset
 
+def divide_logs_into_batches(logs: List[dict]):
+    """
+    Yield successive batches from a list of log events, according to sizing limitations
+    imposed by the EEC: 5 MB payload, 50,000 events
+
+    :param logs: The list of log events
+    """
+    events_left = len(logs)
+    events = deque(logs)
+
+    batch = []
+    batch_size = 0
+    batch_items = 0
+
+    while events_left > 0:
+        if batch_items == MAX_LOG_EVENTS_PER_REQUEST:
+            yield batch
+            batch = []
+            batch_size = 0
+            batch_items = 0
+            continue
+
+        event = events.popleft()
+        events_left -= 1
+
+        if event is not None:
+            event = json.dumps(event).encode("utf-8")
+            event_size = len(event)
+
+            if batch_size + event_size >= MAX_LOG_REQUEST_SIZE:
+                yield batch
+                batch = [event]
+                batch_size = event_size
+                batch_items = 1
+            else:
+                batch.append(event)
+                batch_size += event_size
+                batch_items += 1
+    else:
+        yield batch
 
 @dataclass
 class MintResponse:

dynatrace_extension/sdk/extension.py
@@ -8,6 +8,7 @@ import signal
 import sys
 import threading
 import time
+from collections import deque
 from argparse import ArgumentParser
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime, timedelta, timezone
@@ -996,14 +997,16 @@ class Extension:
             self._metrics.extend(lines)
 
     def _send_events_internal(self, events: Union[dict, List[dict]]):
-        response = self._client.send_events(events, self.log_event_enrichment)
-        with self._internal_callbacks_results_lock:
-            self._internal_callbacks_results[self._send_events.__name__] = Status(StatusValue.OK)
-            if not response or "error" not in response or "message" not in response["error"]:
-                return
-            self._internal_callbacks_results[self._send_events.__name__] = Status(
-                StatusValue.GENERIC_ERROR, response["error"]["message"]
-            )
+        responses = self._client.send_events(events, self.log_event_enrichment)
+
+        for response in responses:
+            with self._internal_callbacks_results_lock:
+                self._internal_callbacks_results[self._send_events.__name__] = Status(StatusValue.OK)
+                if not response or "error" not in response or "message" not in response["error"]:
+                    return
+                self._internal_callbacks_results[self._send_events.__name__] = Status(
+                    StatusValue.GENERIC_ERROR, response["error"]["message"]
+                )
 
     def _send_events(self, events: Union[dict, List[dict]]):
         self._internal_executor.submit(self._send_events_internal, events)

commit 1b7fa87a1e67e7d7eefba6c428f7499a6a58a4ff

Encoding error (#60)
* guard setup.py version finding with a try catch

* Use extension version instead of sdk

* Bumpo version

* SDK packs platform specific wheel #32

Add documentation about platform specific wheel files and build best practices

* add building to index

* Fix issue with events processing

mureq doesn't accept json, events must be bytes, this was broken by #50

* Add try catch guard when sending events

* Stop encoding individual items, need to encode the entire list

dynatrace_extension/__about__.py
@@ -2,5 +2,4 @@
 #
 # SPDX-License-Identifier: MIT
 
-
-__version__ = "1.1.17"
+__version__ = "1.1.18"

dynatrace_extension/sdk/communication.py
@@ -484,7 +484,6 @@ def divide_logs_into_batches(logs: list[dict]):
         events_left -= 1
 
         if event is not None:
-            event = json.dumps(event).encode("utf-8")
             event_size = len(event)
 
             if batch_size + event_size >= MAX_LOG_REQUEST_SIZE: